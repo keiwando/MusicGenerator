b0efdebbac02c06316cdd7976c86876c97dc5940
Keiwan Donyagard <keiwan@gmx.de>
2016-04-03 23:24:39 +0200

    c

--- MusicGenerator.py
+++ MusicGenerator.py
@@ -232,15 +232,16 @@ def getNextMelodyOrChord(melody):
 		counter += 1
 		while last != ">" and len(melody) > 0 and counter < maxNotesInChord:
 			nextNote = melody.pop()
+
+			result += " " + nextNote
+			last = nextNote[-1:]
+			counter += 1
+
 			if len(melody) > 0:
 				peek = melody[-1]
 
 				if peek[0:1] == "<":
 					result += ">"
-
-			result += " " + nextNote
-			last = nextNote[-1:]
-			counter += 1
 
 			if (len(melody) == 0 and last != ">") or counter == maxNotesInChord:
 				result += ">"
